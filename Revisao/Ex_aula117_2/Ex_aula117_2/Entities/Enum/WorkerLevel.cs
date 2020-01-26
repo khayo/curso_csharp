ea2cd5b6b2858c8c560a6f44f125d4ce44684735
Khayo Vannucci <khayovannucci@gmail.com>
2020-01-26 08:46:22 -0300

    Revisão de aulas de lista

---
+++ Revisao/Ex_aula117_2/Ex_aula117_2/Entities/Enum/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ex_aula117_2.Entities.Enum
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
---
+++ Revisao/Listas/Listas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string employee = Console.ReadLine();
+        double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double totalSale = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double salary = baseSalary + (totalSale * 0.15);
+
+        Console.WriteLine("TOTAL = R$ " + salary.ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+}
